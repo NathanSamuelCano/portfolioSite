a55898790ef4400ef8194429c91c8abb81ab4be8
NathanSamuelCano <nathanscano@icloud.com>
2023-09-06 10:37:33 -0700

    Worked on media queries

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const button = document.querySelector('.dropButton')
-button.addEventListener('click', menuToggle)
 
-function menuToggle(){
-    const menu = document.querySelector('.dropDownContent');
-    (menu.style.left === '-400px')? (menu.style.left = '0px') : (menu.style.left = '-400px');
-}
-
+document.querySelector('#dropButton').addEventListener('click', function () {
+    const listItems = document.querySelectorAll(".dropDownContent li");
+    
+    listItems.forEach(function (item, index) {
+      const delay = index * 50; // Delay each item by 50 milliseconds
+      
+      setTimeout(function () {
+        if (item.classList.contains("slide-in")) {
+          item.classList.remove("slide-in"); // Remove the class to reverse the animation
+        } else {
+          item.classList.add("slide-in"); // Add the class to animate
+        }
+      }, delay);
+    });
+  });
