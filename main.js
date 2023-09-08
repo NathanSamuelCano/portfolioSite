2192d0e4c2f01a5cc0c71ec3ce3f95f4f08176ff
NathanSamuelCano <nathanscano@icloud.com>
2023-09-07 20:24:30 -0700

    more work on the mobile designs

--- main.js
+++ main.js
@@ -1,29 +1,58 @@
-// Function to slide in the navigation menu items
-function slideInMenuItems() {
+//function to slide in the navigation menu items
+function slideInMenuItems(){
+  //Crreate variable to eliminate having to write the query 
+  // selection
   const navContent = document.querySelector('#navContent');
+  //create another variable that grabs all of the list items 
+  //inside the #navContent list and return a node list
   const navItems = navContent.querySelectorAll('li');
 
-  // Loop through each list item and apply the slide-in class
+  // So far we have created a variable that stores the nav list
+  //and a variable that stores the li node list
+
+  //function to loop through the node list and apply the slide-in class
+  //so that they can slide in.
   navItems.forEach((item, index) => {
+    // use forEach and setTimeout on each list item to add the slide-in 
+    //class with a delay
     setTimeout(() => {
       item.classList.add('slide-in');
-    }, index * 100); // Delay each item by 100 milliseconds
+      //sets a timer of 100 miliseconds times each li index number
+    }, index *100);
   });
 }
 
-// Function to slide out the navigation menu items
-function slideOutMenuItems() {
+//Same thing but removes the class
+function slideOutMenuItems(){
   const navContent = document.querySelector('#navContent');
-  const navItems = navContent.querySelectorAll('li');
-
-  // Loop through each list item and remove the slide-in class
+  const navItems = navContent.querySelectorAll('li')
   navItems.forEach((item, index) => {
     setTimeout(() => {
       item.classList.remove('slide-in');
-    }, index * 100); // Delay each item by 100 milliseconds
+    }, index * 100);
   });
 }
 
-// Add event listeners to the navButton
-document.querySelector('#navButton').addEventListener('mouseenter', slideInMenuItems);
-document.querySelector('#navButton').addEventListener('mouseleave', slideOutMenuItems);
+//Add event listeners to the navButton
+//Creating variables to eliminate having to write the
+//corresponding information added to nav button only.
+
+const navButton = document.querySelector('#navButton');
+navButton.addEventListener('mouseenter', slideInMenuItems);
+
+// Add event listeners' to the navContent
+//Creating variable to eliminate having to write the 
+//corresponding information
+
+const navContent = document.querySelector('#navContent');
+navContent.addEventListener('mouseleave', slideOutMenuItems)
+// add event listener to keep the menu visible when the mouse is over it
+navContent.addEventListener('mouseenter', () => {
+  //The global clearTimeout() method cancels a timeout
+  //previously established by calling setTimeout() in 
+  //the previous functions.
+  clearTimeout(slideOutTimeout);
+});
+
+//variable to hold the timeout for sliding out classes.
+let slideOutTimeout;
